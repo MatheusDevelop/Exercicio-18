b459a1e137e61002fdae82739a19dbd289058a26
Matheus Barbosa <mathvierabarbosa@gmail.com>
2020-06-16 15:16:20 -0300

    ..

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace jogador
+{
+    class Program
+    {
+        static void Main(string[] args)            
+        {
+           
+            Jogador ribamar = new Jogador();
+            ribamar.Nome= "ribamar";
+            ribamar.Altura = 1.80f;
+            ribamar.Peso = 80f;
+            ribamar.Nascimento = DateTime.Parse("24/06/1887");
+            ribamar.Posicao = "Goleiro";
+            Console.WriteLine(ribamar.CalcularIdade());
+            Console.WriteLine(ribamar.VerificarAposentadoria());
+            
+        }
+    }
+}
